5cc3a680167eca249bd5b58b57a0eb1e8e79d365
ml3424 <ml3424@bs.amalnet.k12.il>
2023-12-19 11:49:11 +0200

    base program

--- app/src/main/java/com/example/hw_tv_ex4/MainActivity.java
+++ app/src/main/java/com/example/hw_tv_ex4/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.hw_tv_ex4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    TextView tV;
+    int counter = 0;
+    int m = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        tV = findViewById(R.id.tV);
+    }
+
+    public void click(View view) {
+        counter ++;
+        if(counter < 7*m)
+        {
+            tV.setText("This is a click number: " + counter);
+        }
+        else
+        {
+            tV.setText("BOOM!");
+            m++;
+        }
     }
 }
